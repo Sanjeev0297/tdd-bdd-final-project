bd7d9e30b5697b6c7c4e8261a03aecdb3120047d
Sanjeev0297 <93074803+Sanjeev0297@users.noreply.github.com>
2024-06-08 22:26:31 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,4 @@
+
 ######################################################################
 # Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
 #
@@ -130,9 +131,24 @@ class TestProductRoutes(TestCase):
         self.assertEqual(new_product["available"], test_product.available)
         self.assertEqual(new_product["category"], test_product.category.name)
 
-        #
-        # Uncomment this code once READ is implemented
-        #
+######################################################################
+# READ A PRODUCT
+######################################################################
+        @app.route("/products/<int:product_id>", methods=["GET"])
+        def get_products(product_id):
+        """
+        Retrieve a single Product
+
+        This endpoint will return a Product based on it's id
+        """
+        app.logger.info("Request to Retrieve a product with id [%s]", product_id)
+
+        product = Product.find(product_id)
+        if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+        app.logger.info("Returning product: %s", product.name)
+        return product.serialize(), status.HTTP_200_OK
 
         # # Check that the location header was correct
         response = self.client.get(location)
